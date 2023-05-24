2dde011abcba501c5fbcb24c6f45eaf76f65f72f
USRVI-LC7 <USRVI-LC7@duoc.acad>
2023-05-24 14:49:16 -0400

    ejemplo funciones

---
+++ ejemplofunciones.py
@@ -0,0 +1,10 @@
+import funciones as fn
+
+#llamando a la función
+fn.saludo("juan")
+nom = input("Ingrese su nombre: ")
+fn.saludo(nom)
+
+aActual = 2023
+aNac = int(input("Ingrese su año Nacimiento"))
+fn.calcularEdad(aActual,aNac)
---
+++ funciones.py
@@ -0,0 +1,9 @@
+# declaración de funciones
+def saludo(nombre):
+    print("Hola")
+    print("Como estas",nombre)
+    print("")
+
+def calcularEdad(añoActual,añoNac):
+    edad = añoActual - añoNac
+    print("Su edad es: ",edad)
